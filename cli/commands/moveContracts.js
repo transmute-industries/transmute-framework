cc2c1d6376dfd788868fc72827f4eff7bee889d6
Orie Steele <orie@ohs.io>
2017-05-13 12:19:35 -0500

    Fix paths

--- cli/commands/moveContracts.js
+++ cli/commands/moveContracts.js
@@ -1,19 +1,16 @@
 
 var fs = require('fs')
 var rimraf = require('rimraf')
+var path = require('path')
 
-let sourceDirectory = '../node_modules/transmute-framework/build/contracts/TransmuteFramework/'
-let destinationDirectory = '../contracts/TransmuteFramework/'
-
-// For Testing
-// let sourceDirectory = '../node_modules/@digix/'
-// let destinationDirectory = '../contracts/@digix/'
+let sourceDirectory = path.resolve(__dirname,  '../../contracts/TransmuteFramework/') 
+let destinationDirectory = path.resolve(__dirname,  '../../../../../contracts/TransmuteFramework/')  
 
 var ncp = require('ncp').ncp
 ncp.limit = 16
 
 const addTransmuteContracts = (_callback) =>{
-    ncp(sourceDirectory, destinationDirectory, _callback)
+    ncp(sourceDirectory + '/', destinationDirectory + '/', _callback)
 }
 
 const removeTransmuteContracts = (_callback) =>{
