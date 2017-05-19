495d7b05d649eb711deca96e77aaa87888bbff81
Eric Olszewski <eolszewski@gmail.com>
2017-05-18 23:34:16 -0500

    Add AddressSetLib test.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,11 +4,11 @@ var Killable = artifacts.require('./zeppelin/lifecycle/Killable.sol')
 var Bytes32SetLib = artifacts.require("./TransmuteFramework/SetLib/Bytes32Set/Bytes32SetLib.sol")
 var Bytes32SetSpec = artifacts.require("./TransmuteFramework/SetLib/Bytes32Set/Bytes32SetSpec.sol")
 
-var IndexedEnumerableSetLib = artifacts.require("./TransmuteFramework/IndexedEnumerableSetLib.sol")
-var TestIndexedEnumerableSetLib = artifacts.require("./TransmuteFramework/TestIndexedEnumerableSetLib.sol")
-
 var AddressSetLib = artifacts.require("./TransmuteFramework/SetLib/AddressSet/AddressSetLib.sol")
 var AddressSetSpec = artifacts.require("./TransmuteFramework/SetLib/AddressSet/AddressSetSpec.sol")
+
+var IndexedEnumerableSetLib = artifacts.require("./TransmuteFramework/IndexedEnumerableSetLib.sol")
+var TestIndexedEnumerableSetLib = artifacts.require("./TransmuteFramework/TestIndexedEnumerableSetLib.sol")
 
 var EventStore = artifacts.require('./EventStore.sol')
 var EventStoreFactory = artifacts.require('./EventStoreFactory.sol')
@@ -23,19 +23,15 @@ module.exports = function(deployer) {
   deployer.link(Killable, EventStore)
   deployer.deploy(EventStore)
 
-
   deployer.deploy(IndexedEnumerableSetLib)
   deployer.link(IndexedEnumerableSetLib, TestIndexedEnumerableSetLib)
   deployer.deploy(TestIndexedEnumerableSetLib)
-
 
   deployer.deploy(Bytes32SetLib)
   deployer.link(Bytes32SetLib, Bytes32SetSpec)
   deployer.deploy(Bytes32SetSpec)
 
-
   deployer.deploy(AddressSetLib)
-
   deployer.link(AddressSetLib, AddressSetSpec)
   deployer.deploy(AddressSetSpec)
 
