e748714e1251a0cae11fbe1d400e9cfd549b2efd
Orie Steele <orie@ohs.io>
2017-05-27 14:40:39 -0500

    First pass on truffle test

--- contracts/TransmuteFramework/EventStoreFactory.sol
+++ contracts/TransmuteFramework/EventStoreFactory.sol
@@ -5,7 +5,7 @@ import "./Utils/StringUtils.sol";
 
 contract EventStoreFactory is EventStore {
   using AddressSetLib for AddressSetLib.AddressSet;
-
+  mapping (address => AddressSetLib.AddressSet) creatorEventStoreMapping;
   AddressSetLib.AddressSet EventStoreAddresses;
 
   // Fallback Function
@@ -13,9 +13,7 @@ contract EventStoreFactory is EventStore {
 
   // Constructor
   function EventStoreFactory() payable {
-    uint eventIndex = solidityEventCount;
-    writeSolidityEvent('FACTORY_CREATED', 1, '');
-    writeSolidityEventProperty(eventIndex, 0, 'ContractOwnerAddress', 'Address', msg.sender, 0, '');
+    writeEvent('FACTORY_CREATED', 'v0', 'Address', tx.origin, 0, '', 0);
   }
 
   // Modifiers
@@ -26,6 +24,11 @@ contract EventStoreFactory is EventStore {
   }
 
   // Helper Functions
+  function getEventStoresByCreator() constant
+    returns (address[])
+  {
+    return creatorEventStoreMapping[msg.sender].values;
+  }
 
   function getEventStores() constant
     returns (address[])
@@ -37,33 +40,26 @@ contract EventStoreFactory is EventStore {
 	function createEventStore() payable
     returns (address)
   {
-    // Validate Local State
- 
-
-    // Update Local State
-
     // Interact With Other Contracts
 		EventStore _newEventStore = new EventStore();
 
     // Update State Dependent On Other Contracts
     EventStoreAddresses.add(address(_newEventStore));
+    creatorEventStoreMapping[msg.sender].add(address(_newEventStore));
 
     uint eventIndex = solidityEventCount;
-
-    writeSolidityEvent('FACTORY_EVENT_STORE_CREATED', 2, '');
-    writeSolidityEventProperty(eventIndex, 0, 'ContractAddress', 'Address', address(_newEventStore), 0, '');
-    writeSolidityEventProperty(eventIndex, 1, 'ContractOwnerAddress', 'Address', msg.sender, 0, '');
-
+    writeEvent('FACTORY_EVENT_STORE_CREATED', 'v0', 'Address', address(_newEventStore), 0, '', 0);
     return address(_newEventStore);
 	}
 
-  function killEventStore(address _address)  {
+  function killEventStore(address _address) checkExistence(_address) {
     // Validate Local State
-    if (this.owner() != msg.sender ) {
+    if (this.owner() != msg.sender || creatorEventStoreMapping[msg.sender].values.length == 0) {
       throw;
     }
 
     // Update Local State
+    creatorEventStoreMapping[msg.sender].remove(_address);
     EventStoreAddresses.remove(_address);
 
     // Interact With Other Contracts
@@ -72,7 +68,6 @@ contract EventStoreFactory is EventStore {
 
     uint eventIndex = solidityEventCount;
 
-    writeSolidityEvent('FACTORY_EVENT_STORE_DESTROYED', 1, StringUtils.uintToBytes(eventIndex));
-    writeSolidityEventProperty(eventIndex, 0, 'ContractAddress', 'Address', _address, 0, '');
+    writeEvent('FACTORY_EVENT_STORE_DESTROYED', 'v0', 'Address', address(_address), 0, '', 0);
   }
 }
--- test/EventStoreFactory/EventStoreFactory.spec.js
+++ test/EventStoreFactory/EventStoreFactory.spec.js
@@ -1,7 +1,7 @@
 var Web3 = require('web3')
 var EventStoreFactory = artifacts.require('./TransmuteFramework/EventStoreFactory.sol')
 var EventStore = artifacts.require('./TransmuteFramework/EventStore.sol')
-var { eventsFromTransaction } = require('../EventStore/SolidityEvent/Transactions')
+
 var _ = require('lodash')
 
 contract('EventStoreFactory', function (accounts) {
@@ -10,42 +10,67 @@ contract('EventStoreFactory', function (accounts) {
   var eventStoreAddress = null
   var eventStoreCreator = accounts[0]
 
-  it('Factory Instance Exists', () => {
-      return EventStoreFactory.deployed().then((_instance) => {
-          factory = _instance
-      })
-  })
+  function toAscii(value) {
+    return web3.toAscii(value).replace(/\u0000/g, '')
+  }
 
-  it('Create EventStore address', (done) => {
-    factory.createEventStore.call({ from: eventStoreCreator }).then((_address) => {
-      eventStoreAddress = _address
-      done()
+  function isVmException(e) {
+    return e.toString().indexOf('VM Exception while') !== -1
+  }
+
+  it('Factory Instance Exists', () => {
+    return EventStoreFactory.deployed().then((_instance) => {
+      factory = _instance
     })
   })
 
-  it('Create EventStore', async () => {
-    let _tx = await factory.createEventStore({ from: eventStoreCreator, gas: 2000000 })
-    let _events = eventsFromTransaction(_tx)
-    assert.equal(_events[0].Type, 'FACTORY_EVENT_STORE_CREATED', 'EventStore Created Event is invalid')
-    assert.equal(_events[1].AddressValue, eventStoreAddress, 'EventStore ContractAddress is invalid')
-    assert.equal(_events[2].AddressValue, eventStoreCreator, 'EventStore ContractOwnerAddress is invalid')
+  describe('createEventStore', () => {
+    it('Create EventStore address', (done) => {
+      factory.createEventStore.call({ from: eventStoreCreator }).then((_address) => {
+        eventStoreAddress = _address
+        done()
+      })
+    })
+    it('Create EventStore', async () => {
+      let _tx = await factory.createEventStore({ from: eventStoreCreator, gas: 2000000 })
+      let event = _tx.logs[0].args
+      // console.log(event)
+
+      let eventType = toAscii(event.Type)
+      assert.equal(eventType, 'FACTORY_EVENT_STORE_CREATED', 'expect first event to be Type FACTORY_EVENT_STORE_CREATED')
+
+      let eventId = event.Id.toNumber()
+      assert.equal(eventId, 1, 'expect FACTORY_EVENT_STORE_CREATED  event to have Id 1, EVENT_STORE_CREATED to have Id 0')
+
+      // Add owner / creator tests here...
+      // let creatorAddress = event.Id.toNumber()
+    })
+
   })
 
-  it('Verify EventStore address contained in EventStoreFactory addresses', async () => {
-    let _addresses = await factory.getEventStores()
-    assert(_.includes(_addresses, eventStoreAddress), 'EventStoreFactory addresses does not contain eventStoreAddress')
+  describe('getEventStores', () => {
+    it('Verify EventStore address contained in EventStoreFactory addresses', async () => {
+      let _addresses = await factory.getEventStores()
+      assert(_.includes(_addresses, eventStoreAddress), 'EventStoreFactory addresses does not contain eventStoreAddress')
+    })
   })
 
-  it('Verify EventStore owner', async () => {
-    let _eventStore = await EventStore.at(eventStoreAddress)
-    let _owner = await _eventStore.owner.call()
-    assert.equal(_owner, factory.address, 'EventStoreFactory is not EventStore owner')
+  describe('owner', () => {
+    it('Verify EventStore owner', async () => {
+      let _eventStore = await EventStore.at(eventStoreAddress)
+      let _owner = await _eventStore.owner.call()
+      assert.equal(_owner, factory.address, 'EventStoreFactory is not EventStore owner')
+    })
   })
 
-  it('Destroy EventStore', async () => {
-    let _tx = await factory.killEventStore(eventStoreAddress, { from: eventStoreCreator })
-    let _events = eventsFromTransaction(_tx)
-    assert.equal(_events[0].Type, 'FACTORY_EVENT_STORE_DESTROYED', 'EventStore Destroyed Event is invalid')
-    assert.equal(_events[1].AddressValue, eventStoreAddress, 'EventStore ContractAddress is invalid')
+  describe('killEventStore', () => {
+    it('Destroy EventStore', async () => {
+      let _tx = await factory.killEventStore(eventStoreAddress, { from: eventStoreCreator })
+      // console.log(_tx)
+      let event = _tx.logs[0].args
+      let eventType = toAscii(event.Type)
+      assert.equal(eventType, 'FACTORY_EVENT_STORE_DESTROYED', 'expect first event to be Type FACTORY_EVENT_STORE_DESTROYED')
+    })
   })
+
 })
