db019a305321e0b1e2464ef893928d1f952c2c1a
Eric Olszewski <eolszewski@gmail.com>
2017-06-20 18:06:25 -0500

    Fix and update SetLib tests. refs #65 (#67)

--- test/SetLib/AddressSetLib.spec.js
+++ test/SetLib/AddressSetLib.spec.js
@@ -1,78 +1,96 @@
-// var Web3 = require('web3')
-// const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-// var AddressSetSpec = artifacts.require('./TransmuteFramework/SetLib/AddressSet/AddressSetSpec.sol')
+var Web3 = require('web3')
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+var AddressSetSpec = artifacts.require('./TransmuteFramework/SetLib/AddressSet/AddressSetSpec.sol')
 
-// contract('AddressSetSpec', function (accounts) {
+contract('addressSet', function(accounts) {
 
-//   var testInstance = null
-//   var size = 0
+  let _addressSet;
+  let size = 0
 
-//   it('test deployed', async () => {
-//     testInstance = await AddressSetSpec.deployed()
-//   })
+  before(async function() {
+    _addressSet = await AddressSetSpec.new();
+  });
 
-//   it('test add, last', async () => {
-//     testInstance.add(accounts[0]).then((_tx) => ++size)
-//     assert.equal(await testInstance.last.call(), accounts[0])
-//     testInstance.add(accounts[1]).then((_tx) => ++size)
-//     assert.equal(await testInstance.last.call(), accounts[1])
-//     testInstance.add(accounts[2]).then((_tx) => ++size)
-//     assert.equal(await testInstance.last.call(), accounts[2])
-//     testInstance.add(accounts[3]).then((_tx) => ++size)
-//     assert.equal(await testInstance.last.call(), accounts[3])
-//     testInstance.add(accounts[4]).then((_tx) => ++size)
-//     assert.equal(await testInstance.last.call(), accounts[4])
-//   })
+  it('test add, last', async () => {
+    await _addressSet.add(accounts[0])
+    ++size
+    assert.equal(await _addressSet.last.call(), accounts[0])
+    await _addressSet.add(accounts[1])
+    ++size
+    assert.equal(await _addressSet.last.call(), accounts[1])
+    await _addressSet.add(accounts[2])
+    ++size
+    assert.equal(await _addressSet.last.call(), accounts[2])
+    await _addressSet.add(accounts[3])
+    ++size
+    assert.equal(await _addressSet.last.call(), accounts[3])
+    await _addressSet.add(accounts[4])
+    ++size
+    assert.equal(await _addressSet.last.call(), accounts[4])
+  })
 
-//   it('test indexOf', async () => {
-//     testInstance.indexOf.call(accounts[0]).then((_index) => assert.equal(_index, 0))
-//     testInstance.indexOf.call(accounts[1]).then((_index) => assert.equal(_index, 1))
-//     testInstance.indexOf.call(accounts[2]).then((_index) => assert.equal(_index, 2))
-//     testInstance.indexOf.call(accounts[3]).then((_index) => assert.equal(_index, 3))
-//     testInstance.indexOf.call(accounts[4]).then((_index) => assert.equal(_index, 4))
-//   })
+  it('test indexOf', async () => {
+    assert.equal(await _addressSet.indexOf.call(accounts[0]), 0)
+    assert.equal(await _addressSet.indexOf.call(accounts[1]), 1)
+    assert.equal(await _addressSet.indexOf.call(accounts[2]), 2)
+    assert.equal(await _addressSet.indexOf.call(accounts[3]), 3)
+    assert.equal(await _addressSet.indexOf.call(accounts[4]), 4)
+  })
 
-//   it('test remove, first', async () => {
-//     assert.equal(await testInstance.first.call(), accounts[0])
-//     testInstance.remove(accounts[0]).then((_tx) => --size)
-//     assert.equal(await testInstance.size.call(), size)
-//     assert.equal(await testInstance.first.call(), accounts[4])
-//     testInstance.remove(accounts[4]).then((_tx) => --size)
-//     assert.equal(await testInstance.size.call(), size)
-//     assert.equal(await testInstance.first.call(), accounts[3])
-//     testInstance.remove(accounts[3]).then((_tx) => --size)
-//     assert.equal(await testInstance.size.call(), size)
-//     assert.equal(await testInstance.first.call(), accounts[2])
-//     testInstance.remove(accounts[2]).then((_tx) => --size)
-//     assert.equal(await testInstance.size.call(), size)
-//     assert.equal(await testInstance.first.call(), accounts[1])
-//     testInstance.remove(accounts[1]).then((_tx) => --size)
-//     assert.equal(await testInstance.size.call(), size)
-//   })
+  it('test remove, first', async () => {
+    assert.equal(await _addressSet.first.call(), accounts[0])
+    await _addressSet.remove(accounts[0])
+    --size
+    assert.equal(await _addressSet.size.call(), size)
+    assert.equal(await _addressSet.first.call(), accounts[4])
+    await _addressSet.remove(accounts[4])
+    --size
+    assert.equal(await _addressSet.size.call(), size)
+    assert.equal(await _addressSet.first.call(), accounts[3])
+    await _addressSet.remove(accounts[3])
+    --size
+    assert.equal(await _addressSet.size.call(), size)
+    assert.equal(await _addressSet.first.call(), accounts[2])
+    await _addressSet.remove(accounts[2])
+    --size
+    assert.equal(await _addressSet.size.call(), size)
+    assert.equal(await _addressSet.first.call(), accounts[1])
+    await _addressSet.remove(accounts[1])
+    --size
+    assert.equal(await _addressSet.size.call(), size)
+  })
 
-//   it('test add, remove, contains', async () => {
-//     await testInstance.add(accounts[5]).then((_tx) => ++size)
-//     await testInstance.add(accounts[6]).then((_tx) => ++size)
-//     await testInstance.add(accounts[7]).then((_tx) => ++size)
-//     await testInstance.add(accounts[8]).then((_tx) => ++size)
-//     await testInstance.add(accounts[9]).then((_tx) => ++size)
-//     assert.equal(await testInstance.contains.call(accounts[5]), true)
-//     assert.equal(await testInstance.contains.call(accounts[6]), true)
-//     assert.equal(await testInstance.contains.call(accounts[7]), true)
-//     assert.equal(await testInstance.contains.call(accounts[8]), true)
-//     assert.equal(await testInstance.contains.call(accounts[9]), true)
-//     await testInstance.remove(accounts[9]).then((_tx) => --size)
-//     await testInstance.remove(accounts[8]).then((_tx) => --size)
-//     await testInstance.remove(accounts[7]).then((_tx) => --size)
-//     assert.equal(await testInstance.contains.call(accounts[7]), false)
-//     assert.equal(await testInstance.contains.call(accounts[8]), false)
-//     assert.equal(await testInstance.contains.call(accounts[9]), false)
-//   })
+  it('test add, remove, contains', async () => {
+    await _addressSet.add(accounts[5])
+    ++size
+    await _addressSet.add(accounts[6])
+    ++size
+    await _addressSet.add(accounts[7])
+    ++size
+    await _addressSet.add(accounts[8])
+    ++size
+    await _addressSet.add(accounts[9])
+    ++size
+    assert.equal(await _addressSet.contains.call(accounts[5]), true)
+    assert.equal(await _addressSet.contains.call(accounts[6]), true)
+    assert.equal(await _addressSet.contains.call(accounts[7]), true)
+    assert.equal(await _addressSet.contains.call(accounts[8]), true)
+    assert.equal(await _addressSet.contains.call(accounts[9]), true)
+    await _addressSet.remove(accounts[9])
+    --size
+    await _addressSet.remove(accounts[8])
+    --size
+    await _addressSet.remove(accounts[7])
+    --size
+    assert.equal(await _addressSet.contains.call(accounts[7]), false)
+    assert.equal(await _addressSet.contains.call(accounts[8]), false)
+    assert.equal(await _addressSet.contains.call(accounts[9]), false)
+  })
 
-//   it('test get, set', async () => {
-//     await testInstance.set(0, accounts[0])
-//     testInstance.get.call(0).then((_value) => assert.equal(_value, accounts[0]))
-//     await testInstance.set(1, accounts[0])
-//     testInstance.get.call(1).then((_value) => assert.equal(_value, accounts[6]))
-//   })
-// })
+  it('test get, set', async () => {
+    await _addressSet.set(0, accounts[0])
+    assert.equal(await _addressSet.get.call(0), accounts[0])
+    await _addressSet.set(1, accounts[0])
+    assert.equal(await _addressSet.get.call(1), accounts[6])
+  })
+});
