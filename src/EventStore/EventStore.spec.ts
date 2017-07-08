6a19d42fb6e7974bfa5f4af6ef4f1e1808a1cde6
Orie Steele <orie@ohs.io>
2017-07-07 23:52:09 -0500

    Remove redundant test

--- src/EventStore/EventStore.spec.ts
+++ src/EventStore/EventStore.spec.ts
@@ -16,26 +16,6 @@ describe('EventStore', () => {
         eventStore = await EventStoreContract.deployed()
     })
 
-    describe('can write UnmarshalledEsCommand and read IFSAEvent', () => {
-        unMarshalledExpectedEvents.forEach((unMarshalledExpectedEvent) => {
-            describe(unMarshalledExpectedEvent.valueType, () => {
-                let eventId
-                it('.writeUnmarshalledEsCommand', async () => {
-                    let tx = await TransmuteFramework.EventStore.writeUnmarshalledEsCommand(eventStore, web3.eth.accounts[0], unMarshalledExpectedEvent)
-                    assert.lengthOf(tx.logs, 1)
-                    assert.equal(tx.logs[0].event, 'EsEvent')
-                    let fsa = TransmuteFramework.EventStore.Common.getFSAFromEventArgs(tx.logs[0].args)
-                    eventId = fsa.meta.id
-                })
-                it('.readFSA', async () => {
-                    let fsa = await TransmuteFramework.EventStore.readFSA(eventStore, web3.eth.accounts[0], eventId)
-                    assert(fsa.meta.id, eventId)
-                    // TODO: Add more tests here...
-                })
-            })
-        })
-    })
-
     describe('can write IFSACommand and read IFSAEvent', () => {
         fsaCommands.forEach((fsac) => {
             describe(fsac.type, () => {
