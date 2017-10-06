047eff83f239cd493796455269c7ed84194e42e7
Orie Steele <orie@ohs.io>
2017-10-06 18:02:37 -0500

    chore(package): update tests

--- smoke-test/node.js
+++ smoke-test/node.js
@@ -1,3 +1,20 @@
-const TransmuteFramework = require('../lib/transmute-framework')
+const T = require('../lib/transmute-framework').default
 
-console.log(TransmuteFramework)
+T.init()
+
+const test = async () => {
+  const accounts = await T.getAccounts()
+  const address = accounts[0]
+  const message = 'hello'
+  const { messageBufferHex, signature } = await T.Toolbox.sign(address, message)
+  let recoveredAddress = await T.Toolbox.recover(address, messageBufferHex, signature)
+  console.log(address)
+  console.log(recoveredAddress)
+  if (address === recoveredAddress) {
+    console.log('Success')
+  } else {
+    console.log('Failure')
+  }
+}
+
+test()
